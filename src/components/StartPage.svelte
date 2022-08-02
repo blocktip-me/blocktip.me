ea2f581a373bab05285ba211c620cbab07966a40
0xpapi <0xpapi@proton.me>
2022-08-02 17:42:28 +0800

    added routing logic for start page form in landing page

--- src/components/StartPage.svelte
+++ src/components/StartPage.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
+	import { walletStore } from '@svelte-on-solana/wallet-adapter-core';
+	import { goto } from '$app/navigation';
 	let username: string;
+
+	async function handleSubmit(event: SubmitEvent) {
+		event.preventDefault();
+
+		if (!$walletStore.connected) {
+			const button: HTMLButtonElement = document.querySelector(
+				'button.wallet-adapter-button'
+			) as HTMLButtonElement;
+
+			button.click();
+		} else {
+			goto(`/create-page?username=${username}`);
+		}
+	}
 </script>
 
 <form
 	class="text-2xl shadow-md rounded-full flex items-center justify-between py-2 px-3"
-	action="/create-page"
+	on:submit={handleSubmit}
 >
 	<div class="p-3">
 		<span class="font-semibold">blocktip.me/</span>
